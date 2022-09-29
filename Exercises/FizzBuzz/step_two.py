20c2459b80ef3dbe1b6428b47222b606bf099716
blessingaliu <blessingaliu@ymail.com>
2022-09-29 17:12:28 +0100

    added code for the first bulletpoint in step_two"

---
+++ Exercises/FizzBuzz/step_two.py
@@ -0,0 +1,27 @@
+# Write a Python program that prints the numbers from 1 to 100.
+# If a number is a multiple of three, print "Fizz" instead of the number.
+# If the number is a multiple of five, print "Buzz" instead of the number.
+# For numbers which are multiples of both three and five, print "FizzBuzz" instead of the number.
+
+
+# If a number is a multiple of 7, print "Bang" instead of the number. 
+# For numbers which are multiples of seven and three / five, append Bang to what you'd have printed anyway. (E.g. 3 * 7 = 21: "FizzBang").
+
+
+for fizzbuzz in range(101): 
+    if  fizzbuzz % 15 == 0 and fizzbuzz % 7 == 0: 
+        print('Fizz' + 'Bang')
+        continue
+    elif fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print('FizzBuzz')
+        continue
+    elif fizzbuzz % 3 == 0:
+        print('Fizz')
+        continue
+    elif fizzbuzz % 5 == 0:
+        print('Buzz')
+        continue
+    elif fizzbuzz % 7 == 0:
+        print('Bang')
+    else:
+        print(fizzbuzz, 'Try again')
